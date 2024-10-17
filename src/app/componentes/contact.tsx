3269bacebbec2ec161d7673625e445b97d309520
GabrielDBri <gabrieldomingos2502@gmail.com>
2024-10-17 00:08:14 -0300

    Ajustes, home, responsividade

--- src/app/componentes/contact.tsx
+++ src/app/componentes/contact.tsx
@@ -37,35 +37,38 @@ export default function Contact() {
   };
 
   return (
-    <div className="container mx-auto my-16 p-6 bg-white shadow-md rounded-lg">
+    <div className="container mx-auto my-16 p-6 bg-black-500 shadow-md rounded-lg text-white">
       <div className="flex flex-col md:flex-row">
         <div className="bg-red-600 text-white p-8 md:w-1/3">
-          <h2 className="text-2xl font-bold mb-4">Vamos conversar!</h2>
-          <p className="mb-4">Se precisar de ajuda, estamos aqui!</p>
+          <h1 className="text-2xl font-bold mb-4">Vamos conversar!</h1>
+          <p className="mb-4">Se precisar de ajuda, estamos aqui para você!</p>
           <ul className="space-y-4">
-            <li>Endereço: 50-460 northern Blvd East, 40, Near Patties South Carolina</li>
-            <li>Telefone: +1 234 567 89</li>
-            <li>Email: contact@firewebsite.com</li>
+            <li><strong>Endereço:</strong> 50-460 northern Blvd East, 40, Near Patties South Carolina</li>
+            <li><strong>Telefone:</strong> +1 234 567 89</li>
+            <li><strong>Email:</strong> contact@firewebsite.com</li>
           </ul>
         </div>
         <div className="md:w-2/3 p-8">
-          <div className="space-y-6">
+          <h2 className="text-xl font-semibold mb-6">Entre em contato conosco</h2>
+          <form className="space-y-6">
             <div className="flex flex-col md:flex-row md:space-x-4">
               <input
                 type="text"
                 name="firstName"
-                placeholder="First name"
-                className="border border-gray-300 rounded-md p-3 w-full"
+                placeholder="Primeiro Nome"
+                className="bg-gray-700 text-white border border-gray-500 rounded-md p-3 w-full mb-4 md:mb-0"
                 value={formData.firstName}
                 onChange={handleChange}
+                aria-label="Primeiro Nome"
               />
               <input
                 type="text"
                 name="lastName"
-                placeholder="Last name"
-                className="border border-gray-300 rounded-md p-3 w-full"
+                placeholder="Último Nome"
+                className="bg-gray-700 text-white border border-gray-500 rounded-md p-3 w-full"
                 value={formData.lastName}
                 onChange={handleChange}
+                aria-label="Último Nome"
               />
             </div>
             <div className="flex flex-col md:flex-row md:space-x-4">
@@ -73,33 +76,35 @@ export default function Contact() {
                 type="email"
                 name="email"
                 placeholder="Email"
-                className="border border-gray-300 rounded-md p-3 w-full"
+                className="bg-gray-700 text-white border border-gray-500 rounded-md p-3 w-full mb-4 md:mb-0"
                 value={formData.email}
                 onChange={handleChange}
+                aria-label="Email"
               />
               <input
                 type="text"
                 name="contato"
                 placeholder="Contato"
-                className="border border-gray-300 rounded-md p-3 w-full"
+                className="bg-gray-700 text-white border border-gray-500 rounded-md p-3 w-full"
                 value={formData.contato}
                 onChange={handleChange}
+                aria-label="Contato"
               />
             </div>
             <div>
-              <p className="mb-2">Qualificação?</p>
-              <div className="flex items-center space-x-4">
-                {['Veiculos Leves', 'Veiculos Pesados', 'Estacionarias', 'Solares'].map((option) => (
-                  <label key={option} className="flex items-center">
+              <h3 className="mb-2">Qualificação?</h3>
+              <div className="flex flex-col md:flex-row md:space-x-4">
+                {['Veículos Leves', 'Veículos Pesados', 'Estacionárias', 'Solares'].map((option) => (
+                  <label key={option} className="flex items-center space-x-2">
                     <input
                       type="radio"
                       name="qualification"
                       value={option}
-                      className="form-radio"
+                      className="form-radio bg-gray-700 text-white"
                       checked={formData.qualification === option}
                       onChange={handleChange}
                     />
-                    <span className="ml-2">{option}</span>
+                    <span>{option}</span>
                   </label>
                 ))}
               </div>
@@ -107,18 +112,20 @@ export default function Contact() {
             <textarea
               name="message"
               placeholder="Sua mensagem"
-              className="border border-gray-300 rounded-md p-3 w-full h-32"
+              className="bg-gray-700 text-white border border-gray-500 rounded-md p-3 w-full h-32"
               value={formData.message}
               onChange={handleChange}
+              aria-label="Mensagem"
             />
             <button
               type="button"
-              className="bg-red-600 text-white px-6 py-3 rounded-md font-bold"
+              className="bg-red-600 text-white px-6 py-3 rounded-md font-bold hover:bg-red-700"
               onClick={handleEnviar}
+              aria-label="Enviar para WhatsApp"
             >
               Enviar para WhatsApp
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
