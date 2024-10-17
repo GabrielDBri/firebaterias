51ee12a7de9caef4086677570dd6a7f9bf3b3b1d
GabrielDBri <gabrieldomingos2502@gmail.com>
2024-10-17 00:15:04 -0300

    Ajustes, home, responsividade teste

--- src/app/componentes/Home/Manifesto.tsx
+++ src/app/componentes/Home/Manifesto.tsx
@@ -1,10 +1,18 @@
-// components/manifesto/Manifesto.tsx
+import Image from 'next/image';
+
 const Manifesto = () => {
   return (
     <section className="py-12 bg-red-600 text-white text-center">
       <h2 className="text-2xl font-bold mb-4">Our Manifesto</h2>
       <div className="max-w-3xl mx-auto">
-        <img src="/images/car.jpg" alt="Car Image" className="w-full h-auto object-cover rounded-md" />
+        <Image 
+          src="/images/car.jpg" 
+          alt="Car Image" 
+          width={600} 
+          height={400} 
+          layout="responsive" 
+          className="rounded-md" 
+        />
       </div>
     </section>
   );
