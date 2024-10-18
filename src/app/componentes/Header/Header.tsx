08eac8da6528c973e0fdef57799039bc0937d5af
GabrielDBri <gabrieldomingos2502@gmail.com>
2024-10-17 23:36:09 -0300

    Ajustes nome pastas

--- src/app/componentes/Header/Header.tsx
+++ src/app/componentes/Header/Header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
-import MainMenu from "./MainMenu";
-import MobileMenu from "./MobileMenu";
+import MainMenu from "../Header/MainMenu";
+import MobileMenu from "../Header/MobileMenu";
 
 export default function Header() {
   return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "./componentes/Header/Headerr";
+import Header from "./componentes/Header/Header";
 import Footer from "./componentes/Footer/Footer";
 import ScrollToTop from "./componentes/ScrollToTop";
 import WhatsAppButton from "./componentes/BotaoWhats";
